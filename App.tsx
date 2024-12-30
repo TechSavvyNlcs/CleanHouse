f9c92346f0e9359ec6c182c33311daacff9b1da3
biwxo71 <biwxo71@gmail.com>
2024-12-30 11:12:24 +0900

    First Commit_Bitna

--- App.tsx
+++ App.tsx
@@ -82,7 +82,7 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> Test
+            Edit <Text style={styles.highlight}>App.tsx</Text> Test Bitna
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
